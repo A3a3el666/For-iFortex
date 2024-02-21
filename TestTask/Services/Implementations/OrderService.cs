19c39341f68b89f9f9966bd1056f40987d3f03fb
A3a3el666 <maxiboy.2002@mail.ru>
2024-02-21 18:48:47 +0300

    Initial commit

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            Order orderWithLargestSum = null;
+            decimal maxSum = decimal.MinValue;
+
+            foreach (var order in await _context.Orders.ToListAsync())
+            {
+                decimal sum = order.Price * order.Quantity;
+
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    orderWithLargestSum = order;
+                }
+            }
+
+            return orderWithLargestSum;
+        }
+
+
+        public async Task<List<Order>> GetOrders()
+        {
+            var ordersWithQuantityGreaterThanTen = await _context.Orders
+                .Where(order => order.Quantity > 10)
+                .ToListAsync();
+
+            return ordersWithQuantityGreaterThanTen;
+        }
+
+    }
+}
+
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var userWithMostOrders = await _context.Users
+                .Include(u => u.Orders)
+                .Where(u => u.Status == UserStatus.Active)
+                .OrderByDescending(u => u.Orders.Count)
+                .FirstOrDefaultAsync();
+
+            return userWithMostOrders;
+        }
+
+
+        public async Task<List<User>> GetUsers()
+        {
+            var inactiveUsers = _context.Users
+                .Where(user => user.Status == UserStatus.Inactive)
+                .ToList();
+
+            return inactiveUsers;
+        }
+    }
+}
+
+//dotnet ef migrations add testMigration --project D:\TestTask\TestTask\TestTask.csproj
+//dotnet ef database update --project D:\TestTask\TestTask\TestTask.csproj
